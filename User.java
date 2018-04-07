709fba76e50210fc64964dd078eca78030e1d825
DylanKing2018 <37106032+DylanKing2018@users.noreply.github.com>
2018-04-07 15:46:14 +0100

    I can't help myself I'm sorry. User class has get and set methods

--- User.java
+++ User.java
@@ -14,4 +14,59 @@ public class User {
 		this.password = password;
 		this.userType = usereType;
 	}
+	/**
+	 * @return the userID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+	
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the userType
+	 */
+	public int getUserType() {
+		return userType;
+	}
+
+	/**
+	 * @param userType the userType to set
+	 */
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
 }
