a6221bf0697e462ef936a7df6e074f73fab0f897
Shwingbad <38210042+Shwingbad@users.noreply.github.com>
2018-04-09 21:00:45 +0100

    facility constructor + sets and gets

--- Facility.java
+++ Facility.java
@@ -3,6 +3,64 @@
  */
 public class Facility
 {
-
+	private int facilityID;
+	private String facilityName;
+	private double pricePerHour;
+	//private Date decommissionedUntilDate;
 	
+	Facility(int facilityID, String facilityName, double pricePerHour)
+	{
+		this.facilityID = facilityID;
+		this.facilityName = facilityName;
+		this.pricePerHour = pricePerHour;
+	}
+	
+	Facility(int facilityID, String facilityName, double pricePerHour, /*Date decommissionedUntilDate*/)
+	{
+		this.facilityID = facilityID;
+		this.facilityName = facilityName;
+		this.pricePerHour = pricePerHour;
+		/*this.decommissionedUntilDate = decommissionedUntilDate;*/
+	}
+	
+	public void setFacilityID(int facilityID)
+	{
+		this.facilityID = facilityID;
+	}
+	
+	public int getFacilityID()
+	{
+		return facilityID;
+	}
+	
+	public void setFacilityName(String facilityName)
+	{
+		this.facilityName = facilityName;
+	}
+	
+	public String getFacilityName()
+	{
+		return facilityName;
+	}
+	
+	public void setPricePerHour(double pricePerHour)
+	{
+		this.pricePerHour = pricePerHour;
+	}
+	
+	public double getPricePerHour()
+	{
+		return pricePerHour;
+	}
+	/*
+	public void setDecommissionedUntilDate(Date decommissionedUntilDate)
+	{
+		this.decommissionedUntilDate = decommissionedUntilDate;
+	}
+	
+	public Date getDecommissionedUntilDate()
+	{
+		return decommissionedUntilDate;
+	}
+	*/
 }
